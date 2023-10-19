2601927aa235b869ba42c7c93b2a7b79792d9c4c
alptugozates <omeralptugozates@gmail.com>
2023-10-19 19:53:30 +0300

    Proje tamamlandı

--- ip/index.js
+++ ip/index.js
@@ -1,4 +1,5 @@
 //axios import buraya gelecek
+import axios from "axios";
 
 var benimIP;
 
@@ -70,3 +71,80 @@ async function ipAdresimiAl(){
 
 
 //kodlar buraya gelecek
+
+const cards = document.querySelector(".cards");
+const contention = async function (){
+await ipAdresimiAl();
+axios
+.get(`https://apis.ergineer.com/ipgeoapi/${benimIP}`)
+.then ((response) => {
+	cards.appendChild(bilgi(response.data))
+})
+.catch((error)  => {
+	console.log(`Error: ${error}`)
+});
+};
+contention();
+	
+
+ 
+
+
+function bilgi(item){
+const divCard = document.createElement("div");
+divCard.classList.add("card");
+
+const img = document.createElement("img");
+img.src = "https://flagcdn.com/w320/tr.png";
+divCard.append(img);
+
+const divCardİnfo = document.createElement("div");
+divCardİnfo.classList.add("card-info");
+divCard.append(divCardİnfo);
+
+const ipAdresi = document.createElement("h3");
+ipAdresi.classList.add("ip");
+ipAdresi.textContent = item.sorgu;
+divCardİnfo.append(ipAdresi);
+
+const ulkeBilgisi = document.createElement("p");
+ulkeBilgisi.classList.add("ulke");
+ulkeBilgisi.textContent = `${item.ülke} (${item.ülkeKodu})`;
+divCardİnfo.append(ulkeBilgisi);
+
+const enlemBoylam = document.createElement("p");
+enlemBoylam.textContent = `Enlem: ${item.enlem} Boylam: ${item.boylam}`
+divCardİnfo.append(enlemBoylam);
+
+const sehir = document.createElement("p");
+sehir.textContent = `Şehir: ${item.şehir}`;
+divCardİnfo.appendChild(sehir);
+
+const bolgeAdi = document.createElement("p");
+bolgeAdi.textContent = `Bölge adı: ${item.bölgeAdı}`;
+divCardİnfo.appendChild(bolgeAdi);
+
+const bolge = document.createElement("p");
+bolge.textContent = `Bölge: ${item.bölge}`;
+divCardİnfo.appendChild(bolge);
+
+const kita = document.createElement("p");
+kita.textContent = `Kıta: ${item.kıta}`;
+divCardİnfo.appendChild(kita);
+
+const saatDilimi = document.createElement("p");
+saatDilimi.textContent = `Saat Dilimi: ${item.saatdilimi}`;
+divCardİnfo.append(saatDilimi);
+
+const paraBirimi = document.createElement("p");
+paraBirimi.textContent = `Para Birimi: ${item.parabirimi}`;
+divCardİnfo.append(paraBirimi);
+
+const isp = document.createElement("p");
+isp.textContent = `ISP: ${item.isp}`;
+divCardİnfo.append(isp);
+
+
+
+return divCard;
+}
